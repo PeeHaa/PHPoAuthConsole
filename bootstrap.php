138eadffd4eb7d2816e66523ef9fe23d07e1fad6
PeeHaa <info@pieterhordijk.com>
2014-04-02 21:37:02 +0200

    Added RunKeeper service

--- bootstrap.php
+++ bootstrap.php
@@ -142,6 +142,7 @@ if ($version !== null) {
         ->add('Microsoft', $credentials['microsoft']['key'], $credentials['microsoft']['secret'], '\\OAuth\\OAuth2\\Service\\Microsoft')
         ->add('Pocket', $credentials['pocket']['key'], $credentials['pocket']['secret'], '\\OAuth\\OAuth2\\Service\\Pocket')
         ->add('Reddit', $credentials['reddit']['key'], $credentials['reddit']['secret'], '\\OAuth\\OAuth2\\Service\\Reddit')
+        ->add('RunKeeper', $credentials['runkeeper']['key'], $credentials['runkeeper']['secret'], '\\OAuth\\OAuth2\\Service\\RunKeeper')
         ;
 }
 
